45b3b6facec6382ca466c200f9e950b355be3718
suyash08 <suyash08@gmail.com>
2015-04-17 13:00:29 +0530

    First change

--- src/com/smb/main/TestMain.java
+++ src/com/smb/main/TestMain.java
@@ -8,6 +8,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a git intro project");
+		System.out.println("First change");
 	}
 
 }
