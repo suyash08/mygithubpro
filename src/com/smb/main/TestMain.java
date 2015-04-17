378886c33cba0ceefa673d9fd6ec0be4063572a2
suyash08 <suyash08@gmail.com>
2015-04-17 12:45:56 +0530

    Initial commit

---
+++ src/com/smb/main/TestMain.java
@@ -0,0 +1,13 @@
+package com.smb.main;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a git intro project");
+	}
+
+}
