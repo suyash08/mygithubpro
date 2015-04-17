1a19ece2de3be98cd862110014f63ab6c35cfc2e
suyash08 <suyash08@gmail.com>
2015-04-17 13:10:44 +0530

    Third change

--- src/com/smb/main/TestMain.java
+++ src/com/smb/main/TestMain.java
@@ -6,7 +6,6 @@ public class TestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("This is a git intro project");
 		System.out.println("First change");
 		System.out.println("Second change");
